1d54f99f6596cea295d3cfabce6a4ed9120610bc
github username <github email address>
2019-02-03 14:32:38 -0800

    cartoon collections solution

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index {|item, index| puts "#{index+1}. #{item}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+
+def summon_captain_planet(planeteer_calls)
+  return planeteer_calls.collect do |element|
+    element << "!"
+    element.capitalize
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? {|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(snacks)
+  snacks.find{|element| element == "cheddar" || element == "gouda" || element == "camembert"}
   # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+
 end
+
+
+#cheese_types = ["cheddar", "gouda", "camembert"]
